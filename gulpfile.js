4a3e3dcc1289f76ff0876d5ad4e5fcf1b5141ddc
Nevo David <me@nevos.io>
2025-07-09 13:19:32 +0700

    feat: fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,5 @@ task('build:icons', copyIcons);
 
 async function copyIcons() {
 	await copyFile('nodes/Postiz/postiz.png', 'dist/nodes/Postiz/postiz.png');
-	await copyFile('README.md', 'dist/README.md');
-	await copyFile('.npmignore', 'dist/.npmignore');
 	return copyFile('pnpm-lock.yaml', 'dist/pnpm-lock.yaml');
 }
